11aa0e94600a07df77080e024ef743ac3ed5f2f5
Topher Winward <hi@topher.io>
2016-07-09 11:53:39 +0100

    eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   // Add the grunt-mocha-test tasks.
   grunt.loadNpmTasks('grunt-mocha-test');
 
@@ -10,23 +10,23 @@ module.exports = function(grunt) {
           reporter: 'spec',
           captureFile: 'results.txt', // Optionally capture the reporter output to a file
           quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
         },
-        src: ['test/**/*.js']
-      }
-    },    
+        src: ['test/**/*.js'],
+      },
+    },
     uglify: {
       my_target: {
         files: {
-          'builder-decorator.min.js': ['src/builder-decorator.js']
-        }
+          'builder-decorator.min.js': ['src/builder-decorator.js'],
+        },
       },
       options: {
-        preserveComments: 'some'
-      }
-    }
+        preserveComments: 'some',
+      },
+    },
   });
-  
+
   grunt.loadNpmTasks('grunt-contrib-uglify'); // load the given tasks
   grunt.registerTask('default', ['uglify']); // Default grunt tasks maps to grunt
   grunt.registerTask('build', []);
--- src/builder-decorator.js
+++ src/builder-decorator.js
@@ -2,152 +2,151 @@
 //! https://github.com/Winwardo/js-builder-decorator
 
 
-"use strict";
+'use strict';
 
-(function(){
-
+(function () {
     /**
      * Decorate any given object or class with builder functions
      */
-    var BuilderDecorator = function(decorated_, options) {
-        'use strict'; 
-        var _ = require('underscore');
-        var deepFreeze = require('deep-freeze');
+  var BuilderDecorator = function (decorated_, options) {
+    'use strict';
+    var _ = require('underscore');
+    var deepFreeze = require('deep-freeze');
 
         // -----
 
-        var createSafeCopy = function(e, allFieldsMustBeSet) {
-            var copy = cloneObjectWithFunctions(e)      
+    var createSafeCopy = function (e, allFieldsMustBeSet) {
+      var copy = cloneObjectWithFunctions(e);
 
-            if (allFieldsMustBeSet === true) {
-                for (var field in copy) {
-                    copy[field] = null;
-                }
-            }
-
-            return copy;
+      if (allFieldsMustBeSet === true) {
+        for (var field in copy) {
+          copy[field] = null;
         }
-                
-        // -----
+      }
 
-        function cloneObjectWithFunctions(obj) {
-            if (obj === null || typeof obj !== 'object') {
-               return obj;
-            }
-         
-            var temp = obj;
-
-            for (var key in obj) {
-                temp[key] = cloneObjectWithFunctions(obj[key]);
-            }
-         
-            return temp;
-        }
+      return copy;
+    };
 
         // -----
-       
-        var newBuilder = function(decorated_, options_) {
-            if (options_ == undefined) {
-                options_ = {};
-            }
 
-            var decorated;
-            if (decorated_ instanceof Function) {
-                decorated = createSafeCopy(new decorated_(), options_.allFieldsMustBeSet);
-            } else {
-                decorated = createSafeCopy(decorated_, options_.allFieldsMustBeSet);
-            }
+    function cloneObjectWithFunctions(obj) {
+      if (obj === null || typeof obj !== 'object') {
+        return obj;
+      }
+
+      var temp = obj;
+
+      for (var key in obj) {
+        temp[key] = cloneObjectWithFunctions(obj[key]);
+      }
+
+      return temp;
+    }
+
+        // -----
+
+    var newBuilder = function (decorated_, options_) {
+      if (options_ == undefined) {
+        options_ = {};
+      }
+
+      var decorated;
+      if (decorated_ instanceof Function) {
+        decorated = createSafeCopy(new decorated_(), options_.allFieldsMustBeSet);
+      } else {
+        decorated = createSafeCopy(decorated_, options_.allFieldsMustBeSet);
+      }
 
             // -----
 
-            var builderObj = {__builderData: decorated}            
+      var builderObj = { __builderData: decorated };
 
-            var makeSetter = function(fieldName) {
-                return function(fieldData) {
-                    var builderData = _.clone(this.__builderData)
-                    builderData[fieldName] = fieldData;
+      var makeSetter = function (fieldName) {
+        return function (fieldData) {
+          var builderData = _.clone(this.__builderData);
+          builderData[fieldName] = fieldData;
 
-                    var copy = {}
-                    copy = applySetters(copy, decorated)
-                    applyBuilder(copy)
-                    copy.__builderData = builderData;
+          var copy = {};
+          copy = applySetters(copy, decorated);
+          applyBuilder(copy);
+          copy.__builderData = builderData;
 
-                    return copy;
-                }
-            }
+          return copy;
+        };
+      };
 
-            var applySetters = function(builderObj__, decorated__) {
-                var newBuilderObj = cloneObjectWithFunctions(builderObj__);
-                for (var field in decorated__) {
-                    newBuilderObj[field] = makeSetter(field);
-                }
-                return newBuilderObj;
-            }
+      var applySetters = function (builderObj__, decorated__) {
+        var newBuilderObj = cloneObjectWithFunctions(builderObj__);
+        for (var field in decorated__) {
+          newBuilderObj[field] = makeSetter(field);
+        }
+        return newBuilderObj;
+      };
 
-            builderObj = applySetters(builderObj, decorated);
+      builderObj = applySetters(builderObj, decorated);
 
-            var applyBuilder = function(builderObj__) {
-                var newBuilderObj = cloneObjectWithFunctions(builderObj__);
-                newBuilderObj.build = function() {
-                    var that = this;
+      var applyBuilder = function (builderObj__) {
+        var newBuilderObj = cloneObjectWithFunctions(builderObj__);
+        newBuilderObj.build = function () {
+          var that = this;
 
                     // check all fields are set
-                    if (options_.allFieldsMustBeSet) {
-                        var unsetFields = [];
-                        for (var field in that.__builderData) {
-                            var fieldData = that.__builderData[field];
-                            if (fieldData === null || fieldData === undefined) {
-                                unsetFields.push(field);
-                            }
-                        }
-                        
-                        if (unsetFields.length !== 0) {
-                            throw "The following fields were not set: " + unsetFields
-                        }
-                    }
-
-                    var response = {}
-
-                    var makeGetter = function(builderData, w) {
-                        var result;
-                        var bData = builderData[w];
-
-                        if (bData === null) {
-                            result = null
-                        } else {
-                            result = deepFreeze(cloneObjectWithFunctions(builderData[w]));
-                        }
-
-                        return function() {
-                            return result;
-                        }
-                    }
-
-                    for (var field in decorated) {
-                        if (options_.lockFunctionsAfterBuild && decorated[field] instanceof Function) {
-                            response[field] = that.__builderData[field]
-                        } else {
-                            response[field] = makeGetter(that.__builderData, field)
-                        }
-                    }
-
-                    return response
-                }
-                return newBuilderObj;
+          if (options_.allFieldsMustBeSet) {
+            var unsetFields = [];
+            for (var field in that.__builderData) {
+              var fieldData = that.__builderData[field];
+              if (fieldData === null || fieldData === undefined) {
+                unsetFields.push(field);
+              }
             }
-            builderObj = applyBuilder(builderObj)
-            return builderObj
-        }
-        return function() { return newBuilder(decorated_, options) }
+
+            if (unsetFields.length !== 0) {
+              throw 'The following fields were not set: ' + unsetFields;
+            }
+          }
+
+          var response = {};
+
+          var makeGetter = function (builderData, w) {
+            var result;
+            var bData = builderData[w];
+
+            if (bData === null) {
+              result = null;
+            } else {
+              result = deepFreeze(cloneObjectWithFunctions(builderData[w]));
+            }
+
+            return function () {
+              return result;
+            };
+          };
+
+          for (var field in decorated) {
+            if (options_.lockFunctionsAfterBuild && decorated[field] instanceof Function) {
+              response[field] = that.__builderData[field];
+            } else {
+              response[field] = makeGetter(that.__builderData, field);
+            }
+          }
+
+          return response;
+        };
+        return newBuilderObj;
+      };
+      builderObj = applyBuilder(builderObj);
+      return builderObj;
     };
-    
+    return function () { return newBuilder(decorated_, options); };
+  };
+
     // NPM exports
-    if(typeof module !== 'undefined') {
-        module.exports = {BuilderDecorator: BuilderDecorator };
-    }
-    
+  if (typeof module !== 'undefined') {
+    module.exports = { BuilderDecorator };
+  }
+
     // Standard js exports
-    if(typeof window !== 'undefined') {
-        window.BuilderDecorator = BuilderDecorator;
-    }
+  if (typeof window !== 'undefined') {
+    window.BuilderDecorator = BuilderDecorator;
+  }
 })();
